76a1dc3e6602583fa4678210e211b7f11baa6b05
Triana Padilla <mavb654@live.rhul.ac.uk>
2019-02-03 20:35:20 +0000

    removed press preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,7 +6,7 @@ import ProductPagePreview from "./preview-templates/ProductPagePreview";
 import EventsPagePreview from "./preview-templates/EventsPagePreview";
 //import ScoresPagePreview from './preview-templates/ScoresPagePreview'
 import AlbumsPagePreview from "./preview-templates/AlbumsPagePreview";
-import PressPagePreview from "./preview-templates/PressPagePreview";
+//import PressPagePreview from "./preview-templates/PressPagePreview";
 import ContactPagePreview from "./preview-templates/ContactPagePreview";
 
 //import AcercaPagePreview from './preview-templates/es/AcercaPagePreview'
@@ -20,7 +20,7 @@ CMS.registerPreviewTemplate("events", EventsPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
 //CMS.registerPreviewTemplate('scores', ScoresPagePreview)
 CMS.registerPreviewTemplate("albums", AlbumsPagePreview);
-CMS.registerPreviewTemplate("press", PressPagePreview);
+//CMS.registerPreviewTemplate("press", PressPagePreview);
 CMS.registerPreviewTemplate("contact", ContactPagePreview);
 
 //CMS.registerPreviewTemplate('acerca', AcercaPagePreview)
