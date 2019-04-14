309bbe8c111d51857b4f2ce4f2290f500c9b97a3
Triana Padilla <mavb654@live.rhul.ac.uk>
2019-04-14 11:57:07 +0100

    404 page styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,24 @@
 import React from "react";
 import PageHeading from "../components/PageHeading";
+import Layout from "../components/Layout";
 
 const NotFoundPage = () => (
-  <main>
-    <PageHeading title="PAGE NOT FOUND" />
-    <div className="site-gutters site-gutters--offset-header site-gutters--push-footer">
-      <div className="c-404-message">
-        <p>The page requested doesn't exist.</p>
-        <p>
-          Go to the{" "}
-          <a href="/" className="styled-link">
-            homepage.
-          </a>
-        </p>
+  <Layout>
+    <main>
+      <PageHeading title="PAGE NOT FOUND" />
+      <div className="site-gutters site-gutters--offset-header site-gutters--push-footer">
+        <div className="c-404-message">
+          <p>The page requested doesn't exist.</p>
+          <p>
+            Go to the{" "}
+            <a href="/" className="styled-link">
+              homepage.
+            </a>
+          </p>
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
+  </Layout>
 );
 
 export default NotFoundPage;
