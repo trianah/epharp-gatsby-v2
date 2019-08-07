a49537fa0c128700d96f4603dc9546ba72296f14
Triana Padilla <mavb654@live.rhul.ac.uk>
2019-08-07 20:24:58 +0100

    instagram changes

--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -14,9 +14,10 @@ export default class Instagram extends Component {
           sortBy="most-recent"
           target={instafeedTarget}
           template="<a href=&quot;{{link}}&quot;><span><img src=&quot;{{image}}&quot; /></span></a>"
-          userId="7522915823"
+          // userId="7522915823"
+          userId="5345282032"
           clientId="ec05939dd8604914936d5d4a4c2f4663"
-          accessToken="7522915823.ec05939.1867511bcb2b4249aff368f46a3d8824"
+          //accessToken="5345282032.b3d5035.42f05697fbee418e87f21aa883ac9d95"
         />
       </div>
     );
