176753071ab5c4ec46acc298bdcb5d83f6631afb
Triana Padilla <triana@ournameismud.co.uk>
2020-05-03 17:14:07 +0100

    remove product preview import

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,7 @@ import CMS from "netlify-cms";
 
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
-import ProductPagePreview from "./preview-templates/ProductPagePreview";
+// import ProductPagePreview from "./preview-templates/ProductPagePreview";
 import EventsPagePreview from "./preview-templates/EventsPagePreview";
 //import ScoresPagePreview from './preview-templates/ScoresPagePreview'
 import AlbumsPagePreview from "./preview-templates/AlbumsPagePreview";
